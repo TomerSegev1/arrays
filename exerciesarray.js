59d70c7d496a7a11a425861e65861da7ecad65a2
user <tomi.segev@gmail.com>
2022-05-18 09:16:56 +0300

    ex2+3

--- exerciesarray.js
+++ exerciesarray.js
@@ -4,5 +4,21 @@ const SumNumbersArray = (NumbersArray) => {
         return accumulator + number; 
      } ,0);
 }
-console.log(SumNumbersArray([1, 2, 3, 4]))
 
+//ex2
+const StrongNumbersArray = (NumbersArray) => {
+    return NumbersArray.map(number => {
+        for (i = 0; i < number; i++) {
+            return number = number * number
+        }
+    });
+}
+
+//ex3
+const MulNumbersArray = (NumbersArray) => {
+    return NumbersArray.reduce((accumulator, number) => {
+         for (i = 0; i < number; i++) {
+          return number * accumulator
+        }
+    } ,1)
+}
